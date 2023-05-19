20b5fd9e3c270406f6d8fe821fb8d520d95e50e1
riya2509 <riyamars3@gmail.com>
2023-05-19 15:09:34 +0530

    Sending variable city names and giving the output of similar names

--- backend/app/controller/appController.js
+++ backend/app/controller/appController.js
@@ -4,16 +4,9 @@ const appController = {};
 const parsedData = (param) => JSON.parse(JSON.stringify(param));
 
 appController.getCity = (req, res) => {
-  // setTimeout(() => {
-  //   res.send({ message: "API is working." });
-  // }, 5000);
-  const name = req.body;
-
-  // mysql(
-  //   `SELECT master.email,master.name as cust_name,master.password as hashed,master.phoneNum, city.name FROM master , city WHERE city.name LIKE 'jam%'`
-  // )
+  const vName = req.query.name;
   mysql(
-    `SELECT t1.name as city_name, t2.name as country_name, t2.code FROM city AS t1 inner join country as t2 ON t1.CountryCode = t2.code WHERE t1.name LIKE 'jam%';`
+    `SELECT t1.name as city_name, t2.name as country_name, t2.code FROM city AS t1 inner join country as t2 ON t1.CountryCode = t2.code WHERE t1.name LIKE '%${vName}%';`
   )
     .then((response) => {
       const data = parsedData(response);
