e1b4f3fad47d5c4240c2b0e4c2d29d343f984b92
riya2509 <riyamars3@gmail.com>
2023-05-20 11:38:06 +0530

    minor changes

--- backend/app/controller/appController.js
+++ backend/app/controller/appController.js
@@ -1,16 +1,15 @@
 import mysql from "../../databases/database.js";
 
 const appController = {};
-const parsedData = (param) => JSON.parse(JSON.stringify(param));
+// const parsedData = (param) => JSON.parse(JSON.stringify(param));
 
 appController.getCity = (req, res) => {
   const vName = req.query.name;
   mysql(
-    `SELECT t1.name as city_name, t2.name as country_name, t2.code FROM city AS t1 inner join country as t2 ON t1.CountryCode = t2.code WHERE t1.name LIKE '%${vName}%';`
+    `SELECT t1.name as city_name, t2.name as country_name, t2.code,t2.code2 AS flag FROM city AS t1 inner join country as t2 ON t1.CountryCode = t2.code WHERE t1.name LIKE '%${vName}%';`
   )
     .then((response) => {
-      const data = parsedData(response);
-      res.send(data);
+      res.send({ data: response });
     })
     .catch((e) => {
       console.log(e);
