27cf5b4ba6b434a16b541b42a7f417289107c47b
riya2509 <riyamars3@gmail.com>
2023-05-20 23:21:54 +0530

    Register Integration

--- frontend/src/Components/Authentication/Register.jsx
+++ frontend/src/Components/Authentication/Register.jsx
@@ -1,7 +1,60 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
+import { toast } from "react-hot-toast";
 
 function Register() {
-  return <div>Register</div>;
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [phone, setPhone] = useState("");
+
+  const handleRegister = () => {
+    axios
+      .post("/auth/register", { name, email, password, phone })
+      .then((response) => {
+        console.log(response);
+        const { status, message } = response.data;
+        if (status === 1) {
+          toast.success(message);
+        } else {
+          toast.error(message);
+        }
+      })
+      .catch((e) => {
+        console.log(e);
+        toast.error(`Some issue registering you in`);
+      });
+  };
+
+  return (
+    <div className="Outerdiv">
+      <input
+        type="text"
+        placeholder="Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <input
+        type="number"
+        placeholder="Phone Number"
+        value={phone}
+        onChange={(e) => setPhone(e.target.value)}
+      />
+      <button onClick={handleRegister}>Register</button>
+    </div>
+  );
 }
 
 export default Register;
--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import Login from "./Components/Authentication/Login";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./Components/Home";
+import Register from "./Components/Authentication/Register";
 const router = createBrowserRouter([
   { path: "", element: <Home /> },
   { path: "login", element: <Login /> },
+  { path: "register", element: <Register /> },
 ]);
 
 function Routes() {
