e32be550e802cb46ffedcf9e1f278d64334b51ab
riya2509 <riyamars3@gmail.com>
2023-05-23 22:17:44 +0530

    Passing headers as middleware (interceptor)

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import axios from "axios";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+axios.interceptors.request.use(
+  function (config) {
+    config.headers.Authorization = window.localStorage.getItem("token");
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
+axios.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    // window.localStorage.clear();
+    return Promise.reject(error);
+  }
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
