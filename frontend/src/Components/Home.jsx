c984b507324e0c15359fb62017e9b3be20debee6
riya2509 <riyamars3@gmail.com>
2023-05-23 22:19:11 +0530

    Adding the Filter field

--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./Layouts/Navbar";
+import Filter from "./Filter";
 
 function Home() {
   return (
     <div>
       <Navbar />
+      <Filter />
     </div>
   );
 }
