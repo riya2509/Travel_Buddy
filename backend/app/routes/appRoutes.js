3d9eb37b80df1a99aa577c0f486f8c42654728ae
riya2509 <riyamars3@gmail.com>
2023-06-06 22:59:48 +0530

    chnaging city route from post to get

--- backend/app/routes/appRoutes.js
+++ backend/app/routes/appRoutes.js
@@ -4,7 +4,7 @@ import { isAuthenticated } from "../middlewares/authentication.js";
 const appRoutes = express.Router();
 
 appRoutes.use(isAuthenticated);
-appRoutes.post("/city", appController.getCity);
+appRoutes.get("/city", appController.getCity);
 appRoutes.get("/me", appController.getProfile);
 appRoutes.post("/updateProfile", appController.updateProfile);
 
