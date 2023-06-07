445785228b18a674867ad656ed64c5167fec2937
riya2509 <riyamars3@gmail.com>
2023-06-07 16:44:23 +0530

    complete functionality of autocomplete dropdown

--- frontend/src/Components/LocSearch.jsx
+++ frontend/src/Components/LocSearch.jsx
@@ -5,7 +5,9 @@ import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
 import axios from "axios";
 import "./LocSearch.css";
-import { Autocomplete, TextField } from "@mui/material";
+import Select from "react-select";
+import { Grid } from "@mui/material";
+// import { Autocomplete, TextField } from "@mui/material";
 
 function LocSearch() {
   const AntSwitch = styled(Switch)(({ theme }) => ({
@@ -53,95 +55,74 @@ function LocSearch() {
     },
   }));
   const [cityData, setCityData] = useState([]);
-  const options = cityData.map((val) => {
-    return val.city_name;
-  });
+  const [cityName, setCityName] = useState("");
+  const [destination, setDestination] = useState("");
 
   const searchCity = () => {
     axios
       .get("/api/city")
       .then((response) => {
-        console.log(response.data.data);
+        // console.log(response.data.data);
         setCityData(response.data.data);
       })
       .catch((e) => {
         console.log(e);
       });
   };
-  //   console.log(
-  //     cityData.map((val) => {
-  //       return val.city_name;
-  //     })
-  //   );
 
   useEffect(() => {
     searchCity();
   }, []);
 
-  const [value, setValue] = React.useState(options[0]);
-  const [inputValue, setInputValue] = React.useState("");
+  const cityArray = cityData.map((val) => {
+    return { label: val.city_name, value: val.id };
+  });
+
+  console.log(cityName);
+  console.log(destination);
+  //   console.log(cityData);
 
   return (
     <div className="outerBox">
-      <div>
-        <div>{`value: ${value !== null ? `'${value}'` : "null"}`}</div>
-        <div>{`inputValue: '${inputValue}'`}</div>
-        <br />
-        <Autocomplete
-          value={value}
-          onChange={(event, newValue) => {
-            setValue(newValue);
-          }}
-          inputValue={inputValue}
-          onInputChange={(event, newInputValue) => {
-            setInputValue(newInputValue);
-          }}
-          id="controllable-states-demo"
-          options={options}
-          sx={{ width: 300 }}
-          renderInput={(params) => (
-            <TextField {...params} label="Select City" />
-          )}
-        />
-      </div>
-
       <div className="container">
         <b>Are you travelling?</b>
+        <Stack direction="row" spacing={1} alignItems="center">
+          <Typography>No</Typography>
+          <AntSwitch
+            defaultChecked
+            inputProps={{ "aria-label": "ant design" }}
+          />
+          <Typography>Yes</Typography>
+        </Stack>
       </div>
-      <Stack direction="row" spacing={1} alignItems="center">
-        <Typography>No</Typography>
-        <AntSwitch defaultChecked inputProps={{ "aria-label": "ant design" }} />
-        <Typography>Yes</Typography>
-      </Stack>
-      <br />
-      <div className="currLoc">
-        From
-        <select name="city" id="city">
-          <option value="">Select a City</option>
-          {cityData.map((val, index) => {
-            return (
-              <option value="cityData" key={index}>
-                {val.city_name}
-              </option>
-            );
-          })}
-        </select>
-      </div>
-
-      <br />
-      <div className="destination">
-        To
-        <select name="city" id="city">
-          <option value="">Select a City</option>
-          {cityData.map((val, index) => {
-            return (
-              <option value="cityData" key={index}>
-                {val.city_name}
-              </option>
-            );
-          })}
-        </select>
-      </div>
+      <Grid container spacing={5}>
+        <Grid sm={12} md={6}>
+          <div className="currLoc">
+            <span className="labels">From:</span>
+            <Select
+              className="cityDropdown"
+              isClearable={true}
+              isSearchable={true}
+              placeholder="Select City"
+              options={cityArray}
+              onChange={(e) => setCityName(e.value)}
+            />
+          </div>
+        </Grid>
+        <Grid sm={12} md={6}>
+          <div className="destination">
+            <span className="labels">TO:</span>
+            <Select
+              className="cityDropdown"
+              isClearable={true}
+              isSearchable={true}
+              placeholder="Select City"
+              options={cityArray}
+              onChange={(e) => setDestination(e.value)}
+            />
+          </div>
+        </Grid>
+      </Grid>
     </div>
   );
 }
