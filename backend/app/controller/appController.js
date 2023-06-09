560c5984eb4ff1e35f183e698ca5324e0374810b
riya2509 <riyamars3@gmail.com>
2023-06-09 22:16:00 +0530

    Logic for updating the post

--- backend/app/controller/appController.js
+++ backend/app/controller/appController.js
@@ -32,7 +32,7 @@ appController.getProfile = (req, res) => {
 };
 
 appController.updateProfile = (req, res) => {
-  console.log(req.body.name);
+  console.log(req.body);
   const { name, phoneNum, gender, year, college, id } = req.body;
   mysql(
     `UPDATE master SET  name = '${name}', phoneNum = '${phoneNum}', gender = '${gender}', year = '${year}', college = '${college}' WHERE (id = '${id}');`
@@ -44,7 +44,7 @@ appController.updateProfile = (req, res) => {
       if (data.changedRows > 0) {
         res.send({
           message: `Profile updated successfully`,
-          data: response,
+          data: {},
           status: 2,
         });
       } else {
@@ -56,6 +56,39 @@ appController.updateProfile = (req, res) => {
       res
         .status(500)
         .send({ message: `Some error while updating profile`, status: 0 });
+    });
+};
+
+appController.updateLocation = (req, res) => {
+  const { currLocation, destination, description } = req.body;
+
+  mysql(
+    `UPDATE master SET currLocation = '${currLocation}', destination = '${destination}', description= '${description}' WHERE (id = '${req.id}');`
+  )
+    .then((response) => {
+      const data = parsedData(response);
+
+      console.log(data);
+      if (data.changedRows > 0) {
+        res.send({
+          message: `Successfully posted your travel plan!`,
+          data: {},
+          status: 2,
+        });
+      } else {
+        res.send({
+          message: `No changes are done in travel plan`,
+          data: {},
+          status: 1,
+        });
+      }
+    })
+    .catch((e) => {
+      console.log(e);
+      res.status(500).send({
+        message: `Some error while posting your travel plan!`,
+        status: 0,
+      });
     });
 };
 
